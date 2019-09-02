415d284243f50327f174f5dedcebd182749843ef
Thomas Vance <tom@iamtomvanec.co.uk>
2019-09-02 19:54:43 +0100

    Some pages

--- src/templates/recipe.js
+++ src/templates/recipe.js
@@ -1,7 +1,17 @@
 import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
 
 export default ({ pageContext: { recipe } }) => (
-  <div style={{ width: 960, margin: "4rem auto" }}>
-    <h1>{recipe.recipeName}</h1>
-  </div>
+  <Layout title={recipe.recipeName}>
+    <div style={{ width: 960, margin: "4rem auto" }}>
+      <Link to='/recipes/'>&laquo;All Recipes</Link>
+      <h4>Ingredients</h4>
+      <ul>
+        { recipe.ingredients.map(ingredient => (
+          <li>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
+  </Layout>
 )
